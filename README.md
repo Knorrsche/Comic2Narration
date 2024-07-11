34e9ffff92d4cafa734aea6e5a36534e4637f389
knorrsche <sandro.paval@gmail.com>
2024-07-11 12:26:13 +0200

    Updated README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+## How to setup
+### 1. (optional) Set up a virtual environment
+```bash
+virtualenv .venv
+source .venv/bin/activat
+```
+### 2. Install the required packages
+```bash
+pip install -r requirements.txt
+```
+
+## How to run 
+```bash
+python3 src/main.py
+```
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from Pages.FileInputPage import FileInputPage
-from Pages.ComicDisplayPage import ComicDisplayPage
-from Components.ComicPreprocessor import ComicPreprocessor
-from Components.SpeechBubbleExtractor import SpeechBubbleExtractor
+from src.Pages.FileInputPage import FileInputPage
+from src.Pages.ComicDisplayPage import ComicDisplayPage
+from src.Components.ComicPreprocessor import ComicPreprocessor
+from src.Components.SpeechBubbleExtractor import SpeechBubbleExtractor
+
 
 class Application:
     def __init__(self, root):
@@ -14,23 +15,25 @@ class Application:
 
         self.file_input_screen = FileInputPage(self)
         self.comic_display_screen = None
-        
+
         self.show_file_input_screen()
-        
+
     def show_file_input_screen(self):
         if self.comic_display_screen:
             self.comic_display_screen.frame.pack_forget()
-        
+
         self.file_input_screen.frame.pack(padx=20, pady=20)
-    
-    def show_comic_display_screen(self,comic_preprocessor: ComicPreprocessor):
-        self.file_input_screen.frame.pack_forget()    
-        self.comic_display_screen = ComicDisplayPage(self,comic_preprocessor)
+
+    def show_comic_display_screen(self, comic_preprocessor: ComicPreprocessor):
+        self.file_input_screen.frame.pack_forget()
+        self.comic_display_screen = ComicDisplayPage(self, comic_preprocessor)
+
 
 def main():
     root = tk.Tk()
     app = Application(root)
     root.mainloop()
-    
+
+
 if __name__ == "__main__":
     main()
